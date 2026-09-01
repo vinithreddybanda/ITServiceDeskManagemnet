8030a0218a76cb48359df91656f4ec57e1f33e57
Vinith Reddy Banda <116493221+vinithreddybanda@users.noreply.github.com>
2026-09-01 11:04:59 +0000

    Refactor code structure for improved readability and maintainability

--- ServiceHelpDeskSystem/Capstone.DAL/obj/Debug/net8.0/Capstone.DAL.AssemblyInfo.cs
+++ ServiceHelpDeskSystem/Capstone.DAL/obj/Debug/net8.0/Capstone.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capstone.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1f7fcf6954b73bfa92ce751cf6b183ad3de126")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6fa21cc8a7bf67976f651d11ece55780c27e9a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capstone.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capstone.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
